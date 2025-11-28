6975d7009b909dd478a480eea8582108a4e33364
eyllbsk <155308570+eyllbsk@users.noreply.github.com>
2025-11-28 17:19:48 +0300

    build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,7 @@
-// Root-level Gradle Kotlin DSL file for Flutter + Firebase
+
 
 plugins {
-    // Google Services plugin for Firebase (apply false here)
+    
     id("com.google.gms.google-services") version "4.4.4" apply false
 }
 
